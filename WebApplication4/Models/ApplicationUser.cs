56060cc4f4a3720be09e09f19ae40e0c05d6ad71
LuanLeo <luandtgcs200115@fpt.edu.vn>
2023-02-11 23:18:56 +0700

    login

---
+++ WebApplication4/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebApplication4.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string Name { get; set; }
+        public string ? ProfilePicture { get; set; }
+
+    }
+}
