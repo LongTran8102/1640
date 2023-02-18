d6018c166cc946bb981b005a53f3f878a062f4f4
LongTran8102 <longtpp8102@gmail.com>
2023-02-18 22:53:45 +0700

    Test

---
+++ Project 1640/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Project_1640.Models;
+
+namespace Project_1640.Data
+
+{
+    public class ApplicationDbContext:IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<ApplicationUser> applicationUsers { get; set; }
+        public DbSet<Project_1640.Models.Category> Category { get; set; }
+    }
+}
