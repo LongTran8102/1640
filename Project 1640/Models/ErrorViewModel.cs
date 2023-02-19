10460be4dede9181a1ca0ac8ec559be9238af9bf
LongTran8102 <longtpp8102@gmail.com>
2023-02-19 21:15:11 +0700

    DeleteAll

--- Project 1640/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Project_1640.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
