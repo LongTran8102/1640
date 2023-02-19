dd714fab0938e59bed571ff487c2d6284d3dc595
LongTran8102 <longtpp8102@gmail.com>
2023-02-19 20:10:33 +0700

    New

---
+++ TopicProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TopicProject.Models;
+
+namespace TopicProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
+        {
+
+        }
+
+        public DbSet<Topic> Topics { get; set; }
+    }
+}
---
+++ TopicProject/Models/Topic.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TopicProject.Models
+{
+    public class Topic
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public DateTime ClosureDate { get; set; } = DateTime.Now;
+        public DateTime FinalClosureDate { get; set; } = DateTime.Now;
+    }
+}
