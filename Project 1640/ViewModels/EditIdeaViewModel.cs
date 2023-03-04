6dae8f93f7c6690f536d855ad35df533702f0fcb
LongTran8102 <117885072+LongTran8102@users.noreply.github.com>
2023-03-04 21:37:40 +0700

    Delete Project 1640 directory

--- Project 1640/ViewModels/EditIdeaViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Project_1640.ViewModels
-{
-    public class EditIdeaViewModel : IdeaViewModel
-    {
-        public int ID { get; set; }
-        public string ExsitingFile { get; set; }
-    }
-}
