3985819ebe6af5a95d5be1446c30dcaad445c892
LuanLeo <luandtgcs200115@fpt.edu.vn>
2023-03-23 18:30:53 +0700

    user list

--- Project 1640/Controllers/UsersController.cs
+++ Project 1640/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DocumentFormat.OpenXml.InkML;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Project_1640.Data;
 using Project_1640.Models;
 using Project_1640.ViewModels;
@@ -25,5 +27,61 @@ namespace Project_1640.Controllers
             usersViewModel.UserList = context.applicationUsers.ToList();
             return View(usersViewModel);
         }
+
+        // GET: UsersController
+        // GET: Categories/Edit/5
+        public async Task<IActionResult> Edit(string? id)
+        {
+            if (id == null || context.applicationUsers == null)
+            {
+                return NotFound();
+            }
+
+            var category = await context.applicationUsers.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Categories/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, ApplicationUser user)
+        {
+            if (id != user.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    context.Update(user);
+                    await context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        private bool UserExists(string id)
+        {
+            return context.applicationUsers.Any(e => e.Id == id);
+        }
+
     }
 }
