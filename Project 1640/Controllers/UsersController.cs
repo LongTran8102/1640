424764a68d7e5d9d9812e452b3d225211917b4e9
LongTran8102 <longtpp0801@gmail.com>
2023-03-24 22:55:15 +0700

    CRUD User

--- Project 1640/Controllers/UsersController.cs
+++ Project 1640/Controllers/UsersController.cs
@@ -1,8 +1,11 @@
 ﻿using DocumentFormat.OpenXml.InkML;
 using DocumentFormat.OpenXml.Office2010.Excel;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Project_1640.Data;
 using Project_1640.Models;
@@ -14,11 +17,13 @@ namespace Project_1640.Controllers
     {
         public readonly ApplicationDbContext context;
         public readonly UserManager<IdentityUser> userManager;
+        public readonly RoleManager<IdentityRole> roleManager;
 
-        public UsersController(ApplicationDbContext _context, UserManager<IdentityUser> _userManager)
+        public UsersController(ApplicationDbContext _context, UserManager<IdentityUser> _userManager, RoleManager<IdentityRole> _roleManager)
         {
             context = _context;
             userManager = _userManager;
+            roleManager = _roleManager;
         }
 
         // GET: UsersController
@@ -27,6 +32,8 @@ namespace Project_1640.Controllers
 
             var users = (from user in context.applicationUsers
                          join d in context.Department on user.DepartmentId equals d.DepartmentId
+                         join ur in context.UserRoles on user.Id equals ur.UserId
+                         join r in context.Roles on ur.RoleId equals r.Id
                          select new UsersViewModel
 
                          {
@@ -34,7 +41,7 @@ namespace Project_1640.Controllers
                              FirstName = user.Firstname,
                              LastName = user.Lastname,
                              Email = user.Email,
-                             Roles = userManager.GetRolesAsync(user).Result,
+                             Roles = r.Name,
                              DepartmentName = d.DepartmentName,
 
 
@@ -50,7 +57,7 @@ namespace Project_1640.Controllers
             {
                 return NotFound();
             }
-
+            DropDownList();
             var user = await context.applicationUsers.FindAsync(id);
             var viewUser = new UsersViewModel
 
@@ -59,6 +66,7 @@ namespace Project_1640.Controllers
                 FirstName = user.Firstname,
                 LastName = user.Lastname,
                 Email = user.Email,
+                DepartmentId = user.DepartmentId,
             };
             return View(viewUser);
         }
@@ -66,22 +74,119 @@ namespace Project_1640.Controllers
         // POST: Categories/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(UsersViewModel model, string id)
-        {           
-
+        public async Task<object> Edit(UsersViewModel model, string id)
+        {
+            DropDownList();
             var user = await context.applicationUsers.FindAsync(id);
-            if(user==null)
+            if (user == null)
                 return NotFound();
-            var userWithSameEmail = await context.applicationUsers.FindAsync(model.Email);
-            /*if(userWithSameEmail!=null&& userWithSameEmail.Id != model.UserID)
+            var userWithSameEmail = await userManager.FindByEmailAsync(model.Email);
+            if (userWithSameEmail != null && userWithSameEmail.Id != id)
             {
                 ModelState.AddModelError("Email", "This email is already used");
-                    return View(model);
-            }*/
-            user.Firstname= model.FirstName;
-            user.Lastname= model.LastName;  
-            user.Email= model.Email;
-            await context.SaveChangesAsync();
+                return View(model);
+            }
+            user.UserName = model.Email;
+            user.Firstname = model.FirstName;
+            user.Lastname = model.LastName;
+            user.Email = model.Email;
+            user.DepartmentId = model.DepartmentId;
+            await userManager.UpdateAsync(user);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var user = await context.applicationUsers.FindAsync(id);
+            if (id == null || context.applicationUsers == null)
+            {
+                return NotFound();
+            }
+            DropDownList();
+            foreach (var userid in context.UserRoles)
+            {
+                if (userid.UserId == id)
+                {
+                    var role = userid.RoleId;
+                    var viewUser = new UsersViewModel
+
+                    {
+                        UserID = id,
+                        RoleId = role,
+                    };
+                    return View(viewUser);
+                };
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<object> EditRole(UsersViewModel model, string id)
+        {
+            string userid = "";
+            string roleid = "";
+            DropDownList();           
+            foreach (var user in context.UserRoles)
+            {
+                if (user.UserId == id)
+                {
+                   
+                    roleid=user.RoleId
+                    
+                };
+            }
+            
+            if (userid == id)
+            {
+                model.RoleId = roleid;
+            }
+            model.RoleId = userid;
+            userid.UserId = id;
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(string? id)
+        {
+            if (id == null || context.applicationUsers == null)
+            {
+                return NotFound();
+            }
+
+            var viewUser = (from user in context.applicationUsers
+                            join d in context.Department on user.DepartmentId equals d.DepartmentId
+                            join ur in context.UserRoles on user.Id equals ur.UserId
+                            join r in context.Roles on ur.RoleId equals r.Id
+                            where user.Id == id
+                            select new UsersViewModel
+
+
+                            {
+                                UserID = id,
+                                FirstName = user.Firstname,
+                                LastName = user.Lastname,
+                                Email = user.Email,
+                                Roles = r.Name,
+                                DepartmentName = d.DepartmentName,
+                            });
+            return View(viewUser);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var user = await context.applicationUsers.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            context.applicationUsers.Remove(user);
+
+
+            context.SaveChanges();
             return RedirectToAction("Index");
         }
 
@@ -89,6 +194,20 @@ namespace Project_1640.Controllers
         {
             return context.applicationUsers.Any(e => e.Id == id);
         }
-
+        public void DropDownList()
+        {
+            List<SelectListItem> department = new List<SelectListItem>();
+            foreach (var dep in context.Department)
+            {
+                department.Add(new SelectListItem { Text = dep.DepartmentName, Value = Convert.ToString(dep.DepartmentId) });
+            }
+            ViewBag.DepartList = department;
+            List<SelectListItem> roles = new List<SelectListItem>();
+            foreach (var rol in context.Roles)
+            {
+                roles.Add(new SelectListItem { Text = rol.Name, Value = Convert.ToString(rol.Id) });
+            }
+            ViewBag.RolesList = roles;
+        }
     }
 }
--- Project 1640/ViewModels/UsersViewModel.cs
+++ Project 1640/ViewModels/UsersViewModel.cs
@@ -6,10 +6,14 @@ namespace Project_1640.ViewModels
     {
         public List<ApplicationUser> UserList { get; set; }
         public string UserID { get; set; }
+        public string Username { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public IEnumerable<string> Roles { get; set; }
+        public string Roles { get; set; }
+        public string RoleId { get; set; }
+        public int DepartmentId { get; set; }
         public string DepartmentName { get; set; }
+
     }
 }
